a16c7762b5b81dd774905592f07debba9e2c25cb
khangluong2004 <khangluong2004@gmail.com>
2026-05-24 00:06:02 +1000

    Add perf test

---
+++ SegmentTreeConsole.Tests/MinMaxSumLazySegmentTreePerfTest.cs
@@ -0,0 +1,67 @@
+﻿using SegmentTreeConsole.Factory;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace SegmentTreeConsole.Tests;
+
+[TestFixture]
+public class MinMaxSumLazySegmentTreePerfTest
+{
+    private GenericSegmentTree<int[], int, int[]>[] _minMaxSumSegmentTrees;
+    private MinMaxSumLazySegmentTreeFactory _minMaxSumLazySegmentTreeFactory = new();
+    private int[][][] _dataSets;
+    private int NUM_SETS = 8;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        int seed = 260504;
+        var random = new Random(seed);
+        _dataSets = new int[NUM_SETS][][];
+        for (int i=0; i < NUM_SETS; i++)
+        {
+            int size = (int)Math.Pow(10, i);
+            _dataSets[i] = new int[size][];
+            for (int j=0; j <size; j++)
+            {
+                _dataSets[i][j] = [random.Next(1_000_000_000)];
+            }
+        }
+
+        _minMaxSumSegmentTrees = new GenericSegmentTree<int[], int, int[]>[NUM_SETS];
+
+        for (int i=0; i < NUM_SETS; i++)
+        {
+            _minMaxSumSegmentTrees[i] = _minMaxSumLazySegmentTreeFactory.CreateSegmentTree(_dataSets[i]);
+        }
+    }
+
+    [Test]
+    public void TestUpdatePerf()
+    {
+        double[] times = new double[NUM_SETS];
+        // Warm up to force JIT to compile the path
+        // Run on any SINGLE tree is fine, but run on all trees to ensure no bias
+        for (int i=0; i < NUM_SETS; i++)
+        {
+            int length = _minMaxSumSegmentTrees[i].DataLength;
+            _minMaxSumSegmentTrees[i].UpdateRange(10, (length - 1) / 5, (length - 1) * 4 / 5);
+        }
+        
+
+        for (int i  = 0; i < NUM_SETS; i++)
+        {
+            int length = _minMaxSumSegmentTrees[i].DataLength;
+            var stopWatch = Stopwatch.StartNew();
+            _minMaxSumSegmentTrees[i].UpdateRange(10, (length-1) / 5, (length-1) * 4 / 5);
+            _minMaxSumSegmentTrees[i].UpdateRange(7, (length - 1) / 3, (length - 1) * 2 / 3);
+            _minMaxSumSegmentTrees[i].UpdateRange(9, (length - 1) / 4, (length - 1) / 2);
+            stopWatch.Stop();
+            times[i] = ((double) stopWatch.ElapsedTicks) / Stopwatch.Frequency * 1e6;
+        }
+
+        Assert.Inconclusive(JsonSerializer.Serialize(times));
+    }
+}
